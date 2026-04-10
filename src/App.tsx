711db40c84fecb8388d688a05c5e95e27cbb3506
Slechh <ijopjojokp@gmail.com>
2026-04-10 17:10:36 +0300

    Create Add Task Button And Add Empty-Task Photo

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,31 @@
 import { useState } from "react";
+import { TodoHeader } from "./components/TodoHeader";
 
 function App() {
-  const a = new Date();
+  const [isModalOpen, setIsModalOpen] = useState<boolean>(false);
+
+  const handleOpen = () => setIsModalOpen((prev) => !prev);
   return (
     <div className="min-h-screen">
-      <div className="max-w-5xl mx-auto px-4">
-        <div className="pt-10 flex flex-col gap-4.5">
-          <h1 className="text-[26px] m-auto">TODO LIST</h1>
-          <div className="flex h-10 gap-4">
-            <div className="relative h-full flex-1">
-              <input
-                type="search"
-                className="appearance-none w-full h-full border-[1.5px] border-purple rounded-md outline-none placeholder:text-light-lavender font-inter text-purple pl-4 pr-10 focus:ring-2 focus:ring-light-purple transition-all duration-300"
-                placeholder="Search note..."
-              />
-              <button>
-                <svg className="w-5 h-5 absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none">
-                  <use href="/src/assets/icons/sprite.svg#search-icon" />
-                </svg>
-              </button>
-            </div>
-            <button className="flex items-center gap-7 text-lg text-white bg-purple p-2.5 rounded-md hover:bg-dark-purple hover:[box-shadow:0_0_9px_rgba(108,99,255,0.5)] transition-shadow duration-200">
-              <span>ALL</span>
-              <svg className="w-2 h-2">
-                <use href="/src/assets/icons/sprite.svg#chevron-icon" />
-              </svg>
-            </button>
-            <button className="flex items-center gap-7 text-lg text-white bg-purple p-2 rounded-md hover:bg-dark-purple hover:[box-shadow:0_0_9px_rgba(108,99,255,0.5)] transition-shadow duration-200">
-              <svg className="w-5.5 h-5.5">
-                <use href="/src/assets/icons/sprite.svg#moon-icon" />
-              </svg>
-            </button>
-          </div>
+      <div className="max-w-5xl mx-auto py-10 px-4 flex flex-col min-h-screen">
+        <TodoHeader />
+        <img
+          src="/src/assets/images/empty-tasks.png"
+          alt=""
+          className="w-55.25 h-42.75 mx-auto mt-7.5"
+        />
+        <div className="flex justify-end mt-auto">
+          <button
+            className="bg-purple p-3.5 rounded-full hover:bg-dark-purple [box-shadow:0_0_9px_rgba(108,99,255,0.5)] transition-all duration-300"
+            onClick={handleOpen}
+          >
+            <svg className="w-6 h-6 text-white">
+              <use href="/src/assets/icons/sprite.svg#plus-icon" />
+            </svg>
+          </button>
         </div>
+
+        {isModalOpen && <div className="fixed inset-0 bg-black/60 z-50"></div>}
       </div>
     </div>
   );
---
+++ src/components/TodoHeader.tsx
@@ -0,0 +1,32 @@
+export function TodoHeader() {
+  return (
+    <div className="flex flex-col gap-4.5">
+      <h1 className="text-[26px] m-auto">TODO LIST</h1>
+      <div className="flex h-10 gap-4">
+        <div className="relative h-full flex-1">
+          <input
+            type="search"
+            className="appearance-none w-full h-full border-[1.5px] border-purple rounded-md outline-none placeholder:text-light-lavender font-inter text-purple pl-4 pr-10 focus:ring-2 focus:ring-light-purple transition-all duration-300"
+            placeholder="Search note..."
+          />
+          <button>
+            <svg className="w-5 h-5 absolute right-3 top-1/2 -translate-y-1/2 pointer-events-none">
+              <use href="/src/assets/icons/sprite.svg#search-icon" />
+            </svg>
+          </button>
+        </div>
+        <button className="flex items-center gap-7 text-lg text-white bg-purple p-2.5 rounded-md hover:bg-dark-purple hover:[box-shadow:0_0_9px_rgba(108,99,255,0.5)] transition-all duration-300">
+          <span>ALL</span>
+          <svg className="w-2 h-2">
+            <use href="/src/assets/icons/sprite.svg#chevron-icon" />
+          </svg>
+        </button>
+        <button className="flex items-center gap-7 text-lg text-white bg-purple p-2 rounded-md hover:bg-dark-purple hover:[box-shadow:0_0_9px_rgba(108,99,255,0.5)] transition-all duration-300">
+          <svg className="w-5.5 h-5.5">
+            <use href="/src/assets/icons/sprite.svg#moon-icon" />
+          </svg>
+        </button>
+      </div>
+    </div>
+  );
+}
