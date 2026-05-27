7fd0b6c4723a7d58100444d56c9eeea3c5e80c76
Slechh <ijopjojokp@gmail.com>
2026-05-27 17:50:06 +0300

    Fix: Change Images Source

--- src/components/EmptyNote.tsx
+++ src/components/EmptyNote.tsx
@@ -2,12 +2,12 @@ export function EmptyNote() {
   return (
     <div className="mx-auto flex flex-col gap-5 mt-7.5 items-center justify-center">
       <img
-        src="/src/assets/images/empty-tasks.png"
+        src="/images/empty-tasks.png"
         alt="No notes"
         className="w-55.25 h-42.75 dark:hidden"
       />
       <img
-        src="/src/assets/images/empty-tasks-dark.png"
+        src="/images/empty-tasks-dark.png"
         alt="No notes"
         className="w-55.25 h-42.75 hidden dark:block"
       />
