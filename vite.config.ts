6e2b95cb37cfa3bfb2075dcf7222782732301eb6
Slechh <ijopjojokp@gmail.com>
2026-06-08 11:05:08 +0300

    Fit: Add Able Sharing Web App

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    allowedHosts: ["clear-lushly-postcard.ngrok-free.dev"],
+  },
+});
